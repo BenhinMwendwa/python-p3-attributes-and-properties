912b029f7775d1b3b42c20eac2ac52fabbfaa13d
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-05-26 18:37:51 +0300

    attributes

--- lib/person.py
+++ lib/person.py
@@ -1,19 +1,26 @@
-#!/usr/bin/env python3
 
 APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
+"Admin",
+"Customer Service",
+"Human Resources",
+"ITC",
+"Production",
+"Legal",
+"Finance",
+"Sales",
+"General Management",
+"Research & Development",
+"Marketing",
+"Purchasing"
 ]
 
 class Person:
-    pass
+  def __init__(self,job="sales"):
+    self.job = job
+  def get_job(self):
+     return self._job 
+  def set_job(self, job): 
+     if job not in APPROVED_JOBS:
+        print("Job must be in list of approved jobs.")
+      return
+        self._job = job
